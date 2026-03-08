5a75c0f7ed0b4e3fc662c6d7aaf63801d9ada5e2
Mike Wazovzky <mike.wazovzky@gmail.com>
2026-03-08 16:30:28 +0400

    feat: add branded app icons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="alternate icon" href="{{ asset('favicon.ico') }}">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
